8624ee38c922febaf282a31ac7bc43d3642ce8e5
Kman25 <kevyugi@gmail.com>
2014-12-03 12:42:22 +0900

    Added a way to select and display from the list of values

--- app/app.js
+++ app/app.js
@@ -1,12 +1,19 @@
 var module = angular.module('app', []);
 
-module.controller('fieldController', ['$scope', 
+module.controller('fieldController', 
 	function($scope) {
 		$scope.fieldTypes = [
 			'Date/Time',
 			'IP Address',
-			'Number',
-			'String'
+			'Number'
 		];
+
+		$scope.selected = [];
+
+		$scope.selectField = function() {
+			if ($scope.inputTypes !== undefined) {
+				$scope.selected.push($scope.inputTypes);
+			}
+		}
 	}
-]);
+);
